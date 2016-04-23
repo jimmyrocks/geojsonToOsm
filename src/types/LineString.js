e68278211d54d97c192427a06ebc634f8390605b
Jim McAndrew <jim@loc8.us>
2016-04-23 19:26:37 -0400

    Move LineString to match others

--- src/types/LineString.js
+++ src/types/LineString.js
@@ -1,7 +1,7 @@
 var createElement = require('../createElement');
 
-var lineString = function (tags, geometry, changeset, version) {
-  var way = createElement(lineString.elementType, changeset, version, tags);
+module.exports = function (id, changeset, version, geometry, tags, newIdGenerator) {
+  var way = createElement('way', id, changeset, version, geometry, tags, newIdGenerator);
 
   var returnObject = {
     node: [],
@@ -13,7 +13,8 @@ var lineString = function (tags, geometry, changeset, version) {
   // Go through all the coordinates and create nodes for them
   returnObject.node = geometry.coordinates.map(function (coords) {
     // Create the node
-    var node = createElement('node', changeset, version, {}, coords[1], coords[0]);
+    // TODO: When updating, these nodes may need to be pulled from the interface (think about this for later!)
+    var node = createElement('node', undefined, changeset, undefined, geometry, undefined, newIdGenerator);
     // Add a reference to it
     returnObject.way[0].nd.push({
       'ref': node.id
@@ -24,6 +25,3 @@ var lineString = function (tags, geometry, changeset, version) {
 
   return returnObject;
 };
-
-lineString.elementType = 'way';
-module.exports = lineString;
