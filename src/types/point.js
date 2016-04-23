9529b856d208eecfd0078fc798d1834e47bfa2b2
Jim McAndrew <jim@loc8.us>
2016-04-23 10:38:21 -0400

    Work with other types

--- src/types/point.js
+++ src/types/point.js
@@ -1,9 +1,12 @@
-module.exports = function(tags, geometry, changeset, version) {
+var createElement = require('../createElement');
+
+var point = function (tags, geometry, osmId, changeset) {
   var coords = geometry.coordinates;
-  var node = createElement('node', changeset, version, tags, coords[1], coords[0]);
+  var node = createElement(point.elementType, changeset, version, tags, coords[1], coords[0]);
   return {
-    'node': [
-      node
-    ]
+    'node': [node]
   };
-},
+};
+
+point.elementType = 'node';
+module.exports = point;
