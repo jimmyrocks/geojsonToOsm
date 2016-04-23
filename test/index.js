9e1e8a1dbfff7a0587abb10dff356a78e6fc8e24
Jim McAndrew <jim@loc8.us>
2016-04-23 19:26:55 -0400

    Add my testing scripts and data

---
+++ test/index.js
@@ -0,0 +1,29 @@
+var geojsonToOsm = require('../index');
+var fs = require('fs');
+
+var files = {
+  'multilinestring': {
+    'json': './test/examples/multilinestring.json',
+    'osm': './test/examples/multilinestring.osm'
+  },
+  'point': {
+    'json': './test/examples/point.json'
+  },
+  'line': {
+    'json': './test/examples/line.json'
+  },
+  'polygon': {
+    'json': './test/examples/polygon.json'
+  },
+  'multiline': {
+    'json': './test/examples/multiline.json'
+  }
+};
+
+var xmlType = 'osmChangeCreate';
+var geometries = JSON.parse(fs.readFileSync(files.multiline.json));
+var changeset = 1;
+osmIdField = undefined;
+versionField = 1;
+
+console.log(JSON.stringify(geojsonToOsm(xmlType, changeset, geometries, osmIdField, versionField), null, 2));
