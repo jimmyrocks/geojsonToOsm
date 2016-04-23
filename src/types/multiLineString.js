130e92695a0419905599e3f6fe3aeeb32469855f
Jim McAndrew <jim@loc8.us>
2016-04-23 10:38:06 -0400

    Work with other types

--- src/types/multiLineString.js
+++ src/types/multiLineString.js
@@ -1,4 +1,7 @@
-module.exports = function (tags, geometry, changeset, version) {
+var multiLineString = function (tags, geometry, changeset, version) {
   // Hack this to only do the first of the line strings (this will need to make relations in the future)
   return types.polygon(tags, geometry, changeset, version);
 };
+
+multiLineString.elementType = 'relation';
+module.exports = multiLineString;
