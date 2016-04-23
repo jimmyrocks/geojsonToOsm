ba3af561352c3eb327ce42b12deb2541ff70a5d2
Jim McAndrew <jim@loc8.us>
2016-04-23 19:25:48 -0400

    Cleanup (this may require a lot of work)

--- src/types/MultiPolygon.js
+++ src/types/MultiPolygon.js
@@ -1,7 +1,27 @@
-module.exports = function (tags, geometry, changeset, version) {
-  // Basically the same thing as a multiLine, but maybe? one level deeper
-  var newGeometry = {
-    coordinates: geometry.coordinates[0]
+var createElement = require('../createElement');
+
+module.exports = function (id, changeset, version, geometry, tags, newIdGenerator) {
+  var way = createElement('way', id, changeset, version, geometry, tags, newIdGenerator);
+
+  var returnObject = {
+    node: [],
+    way: [
+      way
+    ]
   };
-  return types.multilinestring(tags, newGeometry, changeset, version);
+
+  // Go through all the coordinates and create nodes for them
+  returnObject.node = geometry.coordinates.map(function (coords) {
+    // Create the node
+    // TODO: When updating, these nodes may need to be pulled from the interface (think about this for later!)
+    var node = createElement('node', undefined, changeset, undefined, geometry, undefined, newIdGenerator);
+    // Add a reference to it
+    returnObject.way[0].nd.push({
+      'ref': node.id
+    });
+    // Return it back to the array
+    return node;
+  });
+
+  return returnObject;
 };
