fef38bc34f359e9d65a8f1ad86b3de27f22b08ef
Jim McAndrew <jim@loc8.us>
2016-04-23 19:25:23 -0400

    Get multilines working

--- src/types/MultiLineString.js
+++ src/types/MultiLineString.js
@@ -1,7 +1,34 @@
-var multiLineString = function (tags, geometry, changeset, version) {
-  // Hack this to only do the first of the line strings (this will need to make relations in the future)
-  return types.polygon(tags, geometry, changeset, version);
-};
+var createElement = require('../createElement');
+var LineString = require('./LineString');
 
-multiLineString.elementType = 'relation';
-module.exports = multiLineString;
+module.exports = function (id, changeset, version, geometry, tags, newIdGenerator) {
+  var relation = createElement('relation', id, changeset, version, geometry, tags, newIdGenerator);
+
+  var returnObject = {
+    node: [],
+    way: [],
+    relation: [
+      relation
+    ]
+  };
+
+  // Go through all the coordinates and create nodes for them
+  returnObject.way = geometry.coordinates.map(function (subGeometry) {
+    var wayObject = LineString(undefined, changeset, undefined, {
+      coordinates: subGeometry
+    }, undefined, newIdGenerator);
+
+    returnObject.relation[0].member.push({
+      'type': 'way',
+      'ref': wayObject.way[0].id,
+      'role': 'multilinestring'
+    });
+    wayObject.node.forEach(function (node) {
+      returnObject.node.push(node);
+    });
+
+    return wayObject.way[0];
+  });
+
+  return returnObject;
+};
