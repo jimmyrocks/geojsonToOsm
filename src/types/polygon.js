c1e6186e1b67951e7da217c0051ff59c98cb4c1a
Jim McAndrew <jim@loc8.us>
2016-04-23 10:38:23 -0400

    Work with other types

--- src/types/polygon.js
+++ src/types/polygon.js
@@ -1,7 +1,16 @@
-module.exports = function(tags, geometry, changeset, version) {
+var createElement = require('./createElement');
+
+// TODO: Some Polygons can be relations, if they have more than one group of coords, then they're a relation for some reason
+
+var polygon = function(tags, geometry, changeset, version) {
   // Basically the same thing as a line, but one level deeper
   var newGeometry = {
     coordinates: geometry.coordinates[0]
   };
   return types.linestring(tags, newGeometry, changeset, version);
 };
+
+polygon.elementType = function(geometry) {
+ return 'way'
+};
+module.exports = polygon;
