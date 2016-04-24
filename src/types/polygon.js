ddfb2a129a249c77d72411c827b12ab1c315c56f
Jim McAndrew <jim@loc8.us>
2016-04-24 15:25:43 -0400

    Deal with holes

--- src/types/polygon.js
+++ src/types/polygon.js
@@ -1,17 +1,17 @@
 // TODO: Some Polygons can be relations, if they have more than one group of coords, then they're a relation for some reason
-var LineString = require('./LineString');
-var MultiPolygon = require('./MultiPolygon');
+var lineString = require('./lineString');
+var multiPolygon = require('./multiPolygon');
 
 module.exports = function (id, changeset, version, geometry, tags, newIdGenerator) {
   // Basically the same thing as a line, but one level deeper
   var newGeometry = {};
   if (geometry.coordinates.length === 1) {
-    // We can just created it like a LineString
+    // We can just created it like a lineString
     newGeometry.coordinates = geometry.coordinates[0];
-    return LineString(id, changeset, version, newGeometry, tags, newIdGenerator);
+    return lineString(id, changeset, version, newGeometry, tags, newIdGenerator);
   } else {
-    // Otherwise let's treat it like a MultiPolygon so we can keep that logic in one place
+    // Otherwise let's treat it like a multiPolygon so we can keep that logic in one place
     newIdGenerator.coordinates = [geometry.coordinates];
-    return MultiPolygon(id, changeset, version, newGeometry, tags, newIdGenerator);
+    return multiPolygon(id, changeset, version, newGeometry, tags, newIdGenerator);
   }
 };
