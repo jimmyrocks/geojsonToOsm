edbce3b550f5b57d8aa2b98b4091f4ee72647c8f
Jim McAndrew <jim@loc8.us>
2016-04-24 20:12:28 -0400

    Clean up code

--- index.js
+++ index.js
@@ -1,48 +1,15 @@
-var osmTypes = {
-  'Point': require('./src/types/point'),
-  'LineString': require('./src/types/lineString'),
-  'Polygon': require('./src/types/polygon'),
-  'MultiPoint': require('./src/types/multiPoint'),
-  'MultiLineString': require('./src/types/multiLineString'),
-  'MultiPolygon': require('./src/types/multiPolygon')
-};
+var IdGenerator = require('./src/idGenerator');
+var readFeatureCollection = require('./src/readFeatureCollection');
+var js2xml = require('xmljs_trans_js').xmlify;
+var xmlTypes = require('./src/requireTypes')('xmlTypes');
 
-var xmlTypes = {
-  'osmChangeCreate': {},
-  'osmChangeModify': {},
-  'osmChangeRemove': {},
-  'osmXml': {}
-};
-
-var IdGenerator = function (initial, incrementBy) {
-  var types = {};
-  return function (type) {
-    types[type] = types[type] === undefined ? initial : (types[type] + incrementBy);
-    return types[type];
-  };
-};
-
-var readFeatureCollection = function (featureCollection, osmIdField, versionField, changeset, idGenerator) {
-  var geojsonFeatures = featureCollection && featureCollection.features || [];
-  var osmFeatures = [];
-  geojsonFeatures.forEach(function (feature) {
-    if (feature.type === 'Feature') {
-      if (feature.geometry && feature.geometry.type && osmTypes[feature.geometry.type]) {
-        osmFeatures.push(osmTypes[feature.geometry.type](feature[osmIdField], changeset, feature[versionField], feature.geometry, feature.properties, idGenerator));
-      } else {
-        throw new Error('Invalid Geometry Type: ' + feature.geometry.type);
-      }
-    } else if (feature.type === 'FeatureCollection') {
-      osmFeatures.concat(readFeatureCollection(feature, osmIdField, changeset, idGenerator));
-    } else {
-      throw new Error('Invalid Feature Type: ' + feature.type);
-    }
-  });
-  return osmFeatures;
-};
-
-module.exports = function (xmlType, changeset, geojson, osmIdField, versionField) {
+module.exports = function (xmlType, changeset, geojson, options) {
+  options = options || {};
+  var mappedFields = options.mappedFields || {};
   var idGenerator = new IdGenerator(-1, -1);
-  var osmJson = readFeatureCollection(geojson, osmIdField, versionField, changeset, idGenerator);
-  return osmJson;
+  var osmJson = readFeatureCollection(geojson, options, changeset, idGenerator);
+
+  return js2xml(xmlTypes[xmlType](osmJson, options), {
+    'prettyPrint': options.prettyPrint
+  });
 };
--- src/addFeature.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (tags, geometry, changeset, version) {
-  if (types[geometry.type.toLowerCase()]) {
-    return types[geometry.type.toLowerCase()](tags, geometry, changeset, version);
-  } else {
-    return {};
-  }
-};
