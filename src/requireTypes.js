f4522578dc1700814c8da3b900e4af79319dafe7
Jim McAndrew <jim@loc8.us>
2016-05-13 11:50:39 -0600

    Add changes for NPS usage: Update src/requireTypes.js

--- src/requireTypes.js
+++ src/requireTypes.js
@@ -5,10 +5,11 @@ module.exports = function (type, blackList, whiteList) {
   blackList = blackList || [];
   var regexp = new RegExp('(.+?)\.js(on)?$');
   var returnValue = {};
-  fs.readdirSync('./src/' + type).forEach(function (file) {
+  fs.readdirSync(path.resolve(path.join(__dirname, './' + type))).forEach(function (file) {
+    console.log('file', file);
     var match = file.match(regexp);
     if (match && blackList.indexOf(file) === -1 && (!whiteList || whiteList.indexOf(file) > -1)) {
-      returnValue[match[1].toLowerCase()] = require(path.resolve(path.join('./src/' + type, file)));
+      returnValue[match[1].toLowerCase()] = require(path.resolve(path.join(__dirname, './' + type, file)));
     }
   });
   return returnValue;
