53f90b6e7263b97d328a32170b8fd32b4bdc63aa
Jim McAndrew <jim@loc8.us>
2016-05-25 12:30:14 -0600

    Accept string or object, run normalized and rewind on the geojson to make it cleaner

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 var IdGenerator = require('./src/idGenerator');
-var readFeatureCollection = require('./src/readFeatureCollection');
 var js2xml = require('xmljs_trans_js').xmlify;
+var normalize = require('geojson-normalize');
+var readFeatureCollection = require('./src/readFeatureCollection');
+var rewind = require('geojson-rewind');
 var xmlTypes = require('./src/requireTypes')('xmlTypes');
 
 module.exports = function (xmlType, changeset, geojson, options) {
   options = options || {};
   var mappedFields = options.mappedFields || {};
   var idGenerator = new IdGenerator(-1, -1);
-  var osmJson = readFeatureCollection(geojson, options, changeset, idGenerator);
+  var geojsonObj = {};
+  try {
+    geojsonObj = typeof geojson === 'string' ? JSON.parse(geojson) : geojson;
+  } catch (e) {
+    e.message = 'Invalid GeoJSON';
+    throw e;
+  }
+
+  // Run some cleanup functions on the GeoJSON
+  geojsonObj = normalize(geojsonObj);
+  geojsonObj = rewind(geojsonObj);
+
+  // Convert to a format that I'm calling osmJson
+  var osmJson = readFeatureCollection(geojsonObj, options, changeset, idGenerator);
 
   return js2xml(xmlTypes[xmlType](osmJson, options), {
     'prettyPrint': options.prettyPrint
