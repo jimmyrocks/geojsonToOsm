aaae4e93e20cab1effce2868e7f00e883d11462f
Jim McAndrew <jim@loc8.us>
2016-06-07 16:29:07 -0400

    Add options

--- src/types/point.js
+++ src/types/point.js
@@ -1,7 +1,7 @@
 var createElement = require('../createElement');
 
-module.exports = function (id, changeset, version, geometry, tags, newIdGenerator) {
-    var node = createElement('node', id, changeset, version, geometry, tags, newIdGenerator);
+module.exports = function (id, changeset, version, geometry, tags, newIdGenerator, options) {
+    var node = createElement('node', id, changeset, version, geometry, tags, newIdGenerator, options);
     return {
       'node': [node]
     };
