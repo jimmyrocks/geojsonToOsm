3875eb2506cf0befbfe0a451d6be92316331d3c2
Jim McAndrew <jim@loc8.us>
2016-06-07 16:34:31 -0400

    Add options

--- src/types/polygon.js
+++ src/types/polygon.js
@@ -2,16 +2,16 @@
 var lineString = require('./lineString');
 var multiPolygon = require('./multiPolygon');
 
-module.exports = function (id, changeset, version, geometry, tags, newIdGenerator) {
+module.exports = function (id, changeset, version, geometry, tags, newIdGenerator, options) {
   // Basically the same thing as a line, but one level deeper
   var newGeometry = {};
   if (geometry.coordinates.length === 1) {
     // We can just created it like a lineString
     newGeometry.coordinates = geometry.coordinates[0];
-    return lineString(id, changeset, version, newGeometry, tags, newIdGenerator);
+    return lineString(id, changeset, version, newGeometry, tags, newIdGenerator, options);
   } else {
     // Otherwise let's treat it like a multiPolygon so we can keep that logic in one place
     newIdGenerator.coordinates = [geometry.coordinates];
-    return multiPolygon(id, changeset, version, newGeometry, tags, newIdGenerator);
+    return multiPolygon(id, changeset, version, newGeometry, tags, newIdGenerator, options);
   }
 };
