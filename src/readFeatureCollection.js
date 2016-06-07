24dc586badf14050e0c00934bdf4faecd67e55b0
Jim McAndrew <jim@loc8.us>
2016-06-07 16:33:12 -0400

    Add options

--- src/readFeatureCollection.js
+++ src/readFeatureCollection.js
@@ -1,6 +1,6 @@
 var osmTypes = require('./requireTypes')('types');
 
-var readFeatureCollection = module.exports = function (featureCollection, options, changeset, idGenerator) {
+var readFeatureCollection = module.exports = function (featureCollection, options, changeset, idGenerator, options) {
   var geojsonFeatures = featureCollection && featureCollection.features || [];
   var osmIdField = options.osmIdField;
   var versionField = options.versionField;
@@ -11,12 +11,12 @@ var readFeatureCollection = module.exports = function (featureCollection, option
     if (feature.type === 'Feature') {
       var geometryType = feature.geometry && feature.geometry.type && osmTypes[feature.geometry.type.toLowerCase()];
       if (geometryType) {
-        osmFeatures.push(geometryType(osmId, changeset, osmVersion, feature.geometry, feature.properties, idGenerator));
+        osmFeatures.push(geometryType(osmId, changeset, osmVersion, feature.geometry, feature.properties, idGenerator, options));
       } else {
         throw new Error('Invalid Geometry Type: ' + feature.geometry.type);
       }
     } else if (feature.type === 'FeatureCollection') {
-      osmFeatures.concat(readFeatureCollection(feature, options, changeset, idGenerator));
+      osmFeatures.concat(readFeatureCollection(feature, options, changeset, idGenerator, options));
     } else {
       throw new Error('Invalid Feature Type: ' + feature.type);
     }
