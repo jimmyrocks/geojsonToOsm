02be3307aac95e079c31bf7f9e13fce1a6c2653c
Jim McAndrew <jim@loc8.us>
2016-06-08 08:46:41 -0600

    Send foreignKey param instead of using options: Update src/types/multiPoint.js

--- src/types/multiPoint.js
+++ src/types/multiPoint.js
@@ -1,9 +1,10 @@
+// TODO: This doesn't have a test and probably doesn't work
 var createElement = require('../createElement');
 
-module.exports = function (tags, geometry, changeset, version) {
+module.exports = function (osmId, foreignKey, osmVersion, changeset, version, geometry, tags, newIdGenerator) {
   var multiPoint = function () {
-    var coords = geometry.coordinates;
-    var node = createElement('node', changeset, version, tags, coords[1], coords[0]);
+    // var coords = geometry.coordinates
+    var node = createElement('node', osmId, foreignKey, osmVersion, changeset, geometry, tags, newIdGenerator);
     return {
       'node': [
         node
