50202f345467b828e2551f62093e126e3965a45e
Jim McAndrew <jim@loc8.us>
2016-06-08 14:29:53 -0600

    New constructor

--- src/types/multiPoint.js
+++ src/types/multiPoint.js
@@ -1,7 +1,7 @@
 // TODO: This doesn't have a test and probably doesn't work
 var createElement = require('../createElement');
 
-module.exports = function (osmId, foreignKey, osmVersion, changeset, version, geometry, tags, newIdGenerator) {
+module.exports = function (osmId, foreignKey, osmVersion, changeset, geometry, tags, newIdGenerator) {
   var multiPoint = function () {
     // var coords = geometry.coordinates
     var node = createElement('node', osmId, foreignKey, osmVersion, changeset, geometry, tags, newIdGenerator);
