003571e6d386418ec3ad6633c9905e71f475c98e
Jim McAndrew <jim@loc8.us>
2016-06-08 08:46:41 -0600

    Send foreignKey param instead of using options: Update src/types/polygon.js

--- src/types/polygon.js
+++ src/types/polygon.js
@@ -2,16 +2,16 @@
 var lineString = require('./lineString');
 var multiPolygon = require('./multiPolygon');
 
-module.exports = function (id, changeset, version, geometry, tags, newIdGenerator, options) {
+module.exports = function (osmId, foreignKey, osmVersion, changeset, version, geometry, tags, newIdGenerator) {
   // Basically the same thing as a line, but one level deeper
   var newGeometry = {};
   if (geometry.coordinates.length === 1) {
     // We can just created it like a lineString
     newGeometry.coordinates = geometry.coordinates[0];
-    return lineString(id, changeset, version, newGeometry, tags, newIdGenerator, options);
+    return lineString(osmId, foreignKey, osmVersion, changeset, newGeometry, tags, newIdGenerator);
   } else {
     // Otherwise let's treat it like a multiPolygon so we can keep that logic in one place
     newIdGenerator.coordinates = [geometry.coordinates];
-    return multiPolygon(id, changeset, version, newGeometry, tags, newIdGenerator, options);
+    return multiPolygon(osmId, foreignKey, osmVersion, changeset, newGeometry, tags, newIdGenerator);
   }
 };
