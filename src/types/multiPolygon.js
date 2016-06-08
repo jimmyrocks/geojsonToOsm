2a65a68ab73995e2f77939b75f32ed539df2d7d7
Jim McAndrew <jim@loc8.us>
2016-06-08 08:46:41 -0600

    Send foreignKey param instead of using options: Update src/types/multiPolygon.js

--- src/types/multiPolygon.js
+++ src/types/multiPolygon.js
@@ -1,8 +1,8 @@
 var createElement = require('../createElement');
 var lineString = require('./lineString');
 
-module.exports = function (id, changeset, version, geometry, tags, newIdGenerator, options) {
-  var relation = createElement('relation', id, changeset, version, geometry, tags, newIdGenerator, options);
+module.exports = function (osmId, foreignKey, osmVersion, changeset, version, geometry, tags, newIdGenerator) {
+  var relation = createElement('relation', osmId, foreignKey, osmVersion, changeset, geometry, tags, newIdGenerator);
 
   var returnObject = {
     node: [],
@@ -19,9 +19,9 @@ module.exports = function (id, changeset, version, geometry, tags, newIdGenerato
       // For Polygons with multiple rings, the first must be the exterior ring and any others must be interior rings or holes.
       // http://geojson.org/geojson-spec.html#polygon
       var outerInner = ways.length > 0 ? 'inner' : 'outer';
-      var wayObject = lineString(undefined, changeset, undefined, {
+      var wayObject = lineString(undefined, undefined, undefined, changeset, {
         coordinates: polygonGeometry
-      }, undefined, newIdGenerator, options);
+      }, undefined, newIdGenerator);
 
       returnObject.relation[0].member.push({
         'type': 'way',
