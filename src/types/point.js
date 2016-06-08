decb9b76559e2b637ab60b6a0c69aab0d40cc3b7
Jim McAndrew <jim@loc8.us>
2016-06-08 14:30:04 -0600

    New: Update src/types/point.js

--- src/types/point.js
+++ src/types/point.js
@@ -1,6 +1,6 @@
 var createElement = require('../createElement');
 
-module.exports = function (osmId, foreignKey, osmVersion, changeset, version, geometry, tags, newIdGenerator) {
+module.exports = function (osmId, foreignKey, osmVersion, changeset, geometry, tags, newIdGenerator) {
   var node = createElement('node', osmId, foreignKey, osmVersion, changeset, geometry, tags, newIdGenerator);
 
   return {
