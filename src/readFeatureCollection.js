ee6b45e3d40b9d9e9c592e0bd36909e1e1b28d33
Jim McAndrew <jim@loc8.us>
2016-06-08 08:46:41 -0600

    Send foreignKey param instead of using options: Update src/readFeatureCollection.js

--- src/readFeatureCollection.js
+++ src/readFeatureCollection.js
@@ -1,22 +1,24 @@
 var osmTypes = require('./requireTypes')('types');
 
-var readFeatureCollection = module.exports = function (featureCollection, options, changeset, idGenerator, options) {
+var readFeatureCollection = module.exports = function (featureCollection, options, changeset, idGenerator) {
   var geojsonFeatures = featureCollection && featureCollection.features || [];
   var osmIdField = options.osmIdField;
   var versionField = options.versionField;
+  var foreignKeyField = options.foreignKeyField;
   var osmFeatures = [];
   geojsonFeatures.forEach(function (feature) {
     var osmId = feature[osmIdField];
     var osmVersion = feature[versionField]
+    var foreignKey = feature[foreignKeyField];
     if (feature.type === 'Feature') {
       var geometryType = feature.geometry && feature.geometry.type && osmTypes[feature.geometry.type.toLowerCase()];
       if (geometryType) {
-        osmFeatures.push(geometryType(osmId, changeset, osmVersion, feature.geometry, feature.properties, idGenerator, options));
+        osmFeatures.push(geometryType(osmId, foreignKey, osmVersion, changeset, feature.geometry, feature.properties, idGenerator));
       } else {
         throw new Error('Invalid Geometry Type: ' + feature.geometry.type);
       }
     } else if (feature.type === 'FeatureCollection') {
-      osmFeatures.concat(readFeatureCollection(feature, options, changeset, idGenerator, options));
+      osmFeatures.concat(readFeatureCollection(feature, options, changeset, idGenerator));
     } else {
       throw new Error('Invalid Feature Type: ' + feature.type);
     }
