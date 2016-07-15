d69a6846a8eeea1add6f216d61dfabadc435d163
Jim McAndrew <jim@loc8.us>
2016-07-15 15:41:07 -0600

    add close polyline and its tests

---
+++ src/closePolyline.js
@@ -0,0 +1,46 @@
+module.exports = function (osmLine) {
+  var osmPoly = {
+    'node': [],
+    'way': []
+  };
+  var nodes = JSON.parse(JSON.stringify(osmLine.node));
+  osmPoly.way = osmLine.way.map(function (way) {
+    var closedWay = closeWay(nodes, way);
+    nodes = closedWay.nodes;
+    return closedWay.way;
+  });
+  osmPoly.node = nodes;
+  return osmPoly;
+};
+
+var closeWay = function (nodes, way) {
+  var findNode = function (id) {
+    var returnValue = {};
+    nodes.forEach(function (node) {
+      if (node.id === id) {
+        returnValue = node;
+      }
+    });
+    return returnValue || {};
+  };
+  // find the first and last node in the obj
+  var firstNode = findNode(way.nd[0].ref);
+  var lastNode = findNode(way.nd[way.nd.length - 1].ref);
+
+  // Determine if the nodes are the same
+  if (firstNode.lat === lastNode.lat && firstNode.lon === lastNode.lon && firstNode.tag.length === lastNode.tag.length) {
+    // Remove the last node from nodes
+    nodes = nodes.filter(function (node) {
+      return node.id !== lastNode.id;
+    });
+    way.nd[way.nd.length - 1].ref = firstNode.id;
+  } else {
+    way.nd.push({
+      'ref': firstNode.id
+    });
+  }
+  return {
+    'nodes': nodes,
+    'way': way
+  };
+};
