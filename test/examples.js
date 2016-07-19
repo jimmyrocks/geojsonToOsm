524428ed0404bc83dffab71617a80c9680980942
Jim McAndrew <jim@loc8.us>
2016-07-19 08:46:39 -0600

    Make part of the easier test format

--- test/examples.js
+++ test/examples.js
@@ -1,34 +1,51 @@
 var geojsonToOsm = require('../index');
 var fs = require('fs');
+var path = require('path');
 
 var files = {
   'nps_multiline': {
-    'json': './test/examples/nps_multiline.json'
+    'json': 'nps_multiline.json'
   },
   'point': {
-    'json': './test/examples/point.json'
+    'json': 'point.json'
   },
   'line': {
-    'json': './test/examples/line.json'
+    'json': 'line.json'
   },
   'polygon': {
-    'json': './test/examples/polygon.json'
+    'json': 'polygon.json'
   },
   'multiline': {
-    'json': './test/examples/multiline.json'
+    'json': 'multiline.json'
   },
   'multiPolygon': {
-    'json': './test/examples/multiPolygon.json'
+    'json': 'multiPolygon.json'
   }
 };
 
-var xmlType = 'changeset';
-var geometries = JSON.parse(fs.readFileSync(files.multiPolygon.json));
-var changeset = 1;
-osmIdField = undefined;
-versionField = 1;
-
-console.log(geojsonToOsm(xmlType, changeset, geometries, {
+var defaults = {
+  'directory': 'examples',
+  'xmlType': 'changeset',
+  'osmIdField': undefined,
+  'versionField': 1,
   'changeType': 'delete',
-  'prettyPrint': 'true'
-}));
+  'prettyPrint': true
+};
+
+module.exports = Object.keys(files).map(function (key) {
+  var metadata = files[key];
+  Object.keys(defaults).forEach(function (field) {
+    metadata[field] = metadata[field] !== undefined ? metadata[field] : defaults[field];
+  });
+  var geometries = JSON.parse(fs.readFileSync(path.join(__dirname, metadata.directory, metadata.json)));
+  return {
+    'name': 'Test for ' + key,
+    'task': geojsonToOsm,
+    'params': [metadata.xmlType, metadata.changeset, geometries, {
+      'changeType': metadata.changeType,
+      'prettyPrint': metadata.prettyPrint
+    }],
+    'operator': 'jstype',
+    'expected': 'string'
+  };
+});
